472a7beb6b0d7f875744c6865f3c2db0d3293012
Kshitij Dhar <39898582+Dhar15@users.noreply.github.com>
2019-11-15 15:45:50 +0530

    Create Server.java

---
+++ TCP Client-Server/Server.java
@@ -0,0 +1,24 @@
+import java.net.*;
+import java.io.*;
+public class Server {
+	public static void main(String[] args) throws Exception {
+		ServerSocket sersock = new ServerSocket(4999);
+		System.out.println("System ready for connection");
+		Socket sock = sersock.accept();
+		System.out.println("Connection is successful and waiting for chatting.");
+		InputStream istream = sock.getInputStream();
+		BufferedReader fileRead = new BufferedReader(new InputStreamReader(istream));
+		String fname = fileRead.readLine();
+		BufferedReader contentRead = new BufferedReader(new FileReader(fname));
+		OutputStream ostream = sock.getOutputStream();
+		PrintWriter pwrite = new PrintWriter(ostream, true);
+		String str;
+		while((str = contentRead.readLine()) != null) 
+			pwrite.println(str);
+		pwrite.close();
+		contentRead.close();
+		fileRead.close();
+		sock.close();
+		sersock.close();
+	}
+}
