f1d20153ea7ba665531fc09dc3b8def29f3847db
Kshitij Dhar <39898582+Dhar15@users.noreply.github.com>
2019-11-15 15:59:39 +0530

    Update Client.java

--- TCP Client-Server/Client.java
+++ TCP Client-Server/Client.java
@@ -1,5 +1,5 @@
-/*Using TCP/IP sockets, write a client-server program to make the client send the file name and to make the server send back the
-   contents of the requested file if present.
+/*Using TCP/IP sockets, a client-server program to make the client type the file name and to make the server send back the
+   contents of the requested file, if present.
    
 Note: Be sure to create a file (say Sample.txt) in the package directory, and pass "Sample.txt" as the file name when prompted to. */
 
